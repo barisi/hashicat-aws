f3fe78c5046a4583f1a0606d6ef2f731634098ac
Barisi Kane <2213347+barisi@users.noreply.github.com>
2023-09-07 20:59:09 +0000

    Added the first tag

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "bgk-trial-org"
     workspaces {
       name = "hashicat-aws"
